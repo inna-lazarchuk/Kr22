665c164420bb1bb7c6ba13cb25d6c939add72f04
Inna Lazarchuk <inna.lazarchuk538@gmail.com>
2025-03-11 10:32:29 +0300

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./components/pages/main/main.component";
+import {CatalogComponent} from "./components/pages/catalog/catalog.component";
+import {OrderComponent} from "./components/pages/order/order.component";
+import {ProductComponent} from "./components/pages/product/product.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'catalog', component: CatalogComponent},
+  {path: 'order', component: OrderComponent},
+  {path: 'product', component: ProductComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { CatalogComponent } from './components/pages/catalog/catalog.component';
+import { ProductComponent } from './components/pages/product/product.component';
+import { OrderComponent } from './components/pages/order/order.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { LongTextPipe } from './pipes/long-text.pipe';
+import { PricePipe } from './pipes/price.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CatalogComponent,
+    ProductComponent,
+    OrderComponent,
+    HeaderComponent,
+    FooterComponent,
+    LongTextPipe,
+    PricePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
